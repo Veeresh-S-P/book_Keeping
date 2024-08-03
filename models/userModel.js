9b7fcb4b4304b45483f8ae6be5c029cd46857684
veeresh-s_p <veereshsp1234@gmail.com>
2024-08-03 10:55:13 +0530

    fixed bug related to creatinng library

--- models/userModel.js
+++ models/userModel.js
@@ -17,7 +17,7 @@ const userSchema = mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['Author', 'Borrower'],
+    enum: ['Author', 'Borrower','Admin'],
     required: true,
   },
 }, {
