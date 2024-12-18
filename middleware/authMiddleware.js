647c12cefe74113dcadf0164015ed2beccd9b447
Veeresh S P <112792652+Veeresh-S-P@users.noreply.github.com>
2024-12-18 12:00:33 +0530

    Update authMiddleware.js
     optimized code

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,12 +3,13 @@ const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 
 const protect = asyncHandler(async (req, res, next) => {
-  let token;
+  const token = req.headers.authorization || req.headers.authorization.startsWith('Bearer');
 
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+  if (token) {
     try {
-      token = req.headers.authorization.split(' ')[1];
+      
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      
       req.user = await User.findById(decoded.id).select('-password');
       next();
     } catch (error) {
